9780f0150056f1abb3297566eedf18a5bfe747a1
tjsphotophactory-droid <tjsphotophactory@gmail.com>
2025-10-29 00:03:40 -0400

    Create Services.js

---
+++ src/pages/Services.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>We specialize in Portrait, Headshot, and Individual Photography sessions.</p>
+    </div>
+  );
+}
