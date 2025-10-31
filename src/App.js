d77701ebb930e570f897a1a57ecec93ecc28d826
tjsphotophactory-droid <tjsphotophactory@gmail.com>
2025-10-31 19:50:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ color: "white", textAlign: "center", marginTop: "50px" }}>
-        TJ’s Photo Phactory
-      </h1>
-      <p style={{ color: "gray", textAlign: "center" }}>
+    <div
+      className="hero"
+      style={{
+        backgroundImage: "url('/hero-bg.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ marginTop: "50px" }}>TJ’s Photo Phactory</h1>
+      <p style={{ color: "gray" }}>
         Welcome to my photography portfolio. Coming soon…
       </p>
     </div>
@@ -15,13 +24,3 @@ function App() {
 }
 
 export default App;
-<div
-  className="hero"
-  style={{
-    backgroundImage: "url('/hero-bg.jpg')",
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    height: '100vh',
-  }}
->
