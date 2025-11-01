eeeed7d78285ccf486ce05f016742a98991185d0
Tajay Williams <tjsphotophactory@gmail.com>
2025-11-01 15:00:44 -0400

    Add working Cloudflare version

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div
-      className="hero"
-      style={{
-        backgroundImage: "url('/hero-bg.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        height: "100vh",
-        color: "white",
-        textAlign: "center",
-      }}
-    >
-      <h1 style={{ marginTop: "50px" }}>TJ’s Photo Phactory</h1>
-      <p style={{ color: "gray" }}>
-        Welcome to my photography portfolio. Coming soon…
-      </p>
+    <div className="App">
+      <div className="hero">
+        <h1>TJ’s Photo Phactory</h1>
+        <p>Capturing Life’s Best Moments – Coming Soon</p>
+      </div>
     </div>
   );
 }
